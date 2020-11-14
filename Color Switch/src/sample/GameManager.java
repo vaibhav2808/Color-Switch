cf5ca4560ae9966504b4220e96835df4413283ec
SamadShahid <73770490+SamadShahid@users.noreply.github.com>
2020-11-14 14:02:32 +0530

    Updated the template

--- Color Switch/src/sample/GameManager.java
+++ Color Switch/src/sample/GameManager.java
@@ -1,2 +1,47 @@
-package sample;public class GameManager {
+package sample;
+
+public class GameManager {
+    private Game game;
+    private int highScore;
+
+    public GameManager(){
+
+    }
+
+    public void setGame(Game g){
+        this.game = g;
+    }
+    public void setHighScore(int h){
+        this.highScore = h;
+    }
+    public Game getGame(){
+        return game;
+    }
+    public int getHighScore(){
+        return highScore;
+    }
+
+    public void startNewGame(){
+
+    }
+
+    public void savedGamesList(){
+
+    }
+
+    public void continuePreviousGame(){
+
+    }
+
+    public void saveGame(){
+
+    }
+
+    public void exitGame(){
+
+    }
+
+    public void playGame(){
+
+    }
 }
