95770ca7ade88900d12072ce9f771543904aeb09
SamadShahid <73770490+SamadShahid@users.noreply.github.com>
2020-11-14 14:05:22 +0530

    Update Player.java

--- Color Switch/src/sample/Player.java
+++ Color Switch/src/sample/Player.java
@@ -1,2 +1,33 @@
-package sample;public class Player {
+package sample;
+
+public class Player {
+    private String name;
+    private int score;
+    private Ball ball;
+
+    public Player(){
+
+    }
+
+    public void setScore(int s){
+        this.score = s;
+    }
+    public int getScore(){
+        return score;
+    }
+    public Ball getBall(){
+        return ball;
+    }
+    
+    public boolean canResurrect(){
+        return false;
+    }
+
+    public void collectStar(){
+
+    }
+
+    public void jump(){
+        
+    }
 }
