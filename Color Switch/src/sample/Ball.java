9f9487e5ad43a05117b4c508b66cb6295d028fe8
SamadShahid <73770490+SamadShahid@users.noreply.github.com>
2020-11-14 14:00:06 +0530

    Updated the template

--- Color Switch/src/sample/Ball.java
+++ Color Switch/src/sample/Ball.java
@@ -1,2 +1,24 @@
-package sample;public class Ball {
+package sample;
+
+public class Ball implements ColorChanging{
+    private String color;
+    private final int radius;
+
+    public Ball(){
+
+    }
+
+    public void setColor(String s){
+        this.color = s;
+    }
+
+    @Override
+    public String getColor(){
+        return "Red";
+    }
+
+    @Override 
+    public void changeColor(){
+
+    }
 }
