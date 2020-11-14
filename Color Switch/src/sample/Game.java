bdf5ec5bf39d488a82c75f579730be6eb65c7344
SamadShahid <73770490+SamadShahid@users.noreply.github.com>
2020-11-14 14:03:35 +0530

    Updates the template

--- Color Switch/src/sample/Game.java
+++ Color Switch/src/sample/Game.java
@@ -1,2 +1,51 @@
-package sample;public class Game {
+package sample;
+
+public class Game{
+    private Ball ball;
+    private Player player;
+    private GameManager manager;
+    private ArrayList<Obstacle> obstacles;
+
+    public Game(){
+
+    }
+
+    public void setManager(GameManager m){
+        this.manager = m;
+    }
+    public int getScore(){
+        return player.getScore();
+    }
+
+    public boolean checkCollision(){
+        return false;
+    }
+
+    public void play(){
+
+    }
+
+    public void exitToMainMenu(){
+
+    }
+
+    public void resumeGame(){
+
+    }
+
+    public void pauseGame(){
+
+    }
+
+    public void restartGame(){
+
+    }
+
+    public void saveGameAndExit(){
+
+    }
+
+    public void continueGame(){
+
+    }
 }
