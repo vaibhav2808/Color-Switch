d16d6207cc2d1e318f7aa7dbc2d37180b5eada70
Vaibhav <vaibhavsoni280801@gmail.com>
2020-11-22 12:37:25 +0530

    Added color switcher

---
+++ Color Switch/src/sample/ColorChangerObstacle.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+
+public class ColorChangerObstacle {
+    private Group group;
+    private final int radius=15;
+    ColorChangerObstacle(){
+        group=new Group();
+        Arc arcs[]=new Arc[4];
+        Paint color[]={Color.DEEPPINK,Color.LIGHTBLUE,Color.YELLOW,Color.DARKVIOLET};
+        for(int i=0;i<4;i++){
+            arcs[i]=new Arc(20,20,radius,radius,90*i,90);
+            arcs[i].setFill(color[i]);
+//            arcs[i].setStrokeWidth(10);
+            arcs[i].setType(ArcType.ROUND);
+            group.getChildren().add(arcs[i]);
+        }
+
+    }
+    public Group getGroup(){
+        return group;
+    }
+}
